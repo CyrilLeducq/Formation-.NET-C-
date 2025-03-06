e0862afaa3b547c7caa6291432b7bf7c547f6dc4
CyrilLEDUCQ <cyril.leducq.pro@gmail.com>
2025-03-06 14:04:48 +0100

    Demo 2 Les props dans react + exercice

--- 18 - React/Demos/01_demo_base/src/App.jsx
+++ 18 - React/Demos/01_demo_base/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FirstComponent from './components/FirstComponent/FirstComponent'
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [count, setCount] = useState(0)
