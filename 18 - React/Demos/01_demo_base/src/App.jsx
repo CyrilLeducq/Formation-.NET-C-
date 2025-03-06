c81407dd1b668dfb3af8384d6d88f4525b9cf92e
CyrilLEDUCQ <cyril.leducq.pro@gmail.com>
2025-03-06 10:54:57 +0100

    Initiation React + exercice

---
+++ 18 - React/Demos/01_demo_base/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FirstComponent from './components/FirstComponent/FirstComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <FirstComponent />
+      {/* <FirstComponent />
+      <FirstComponent />
+      <FirstComponent /> */}
+    </>
+  )
+}
+
+export default App
---
+++ 18 - React/Exercices/exercice_01/exercice_01.md
@@ -0,0 +1,10 @@
+# Exercice React 1
+
+## Objectif
+Créer une application React pour afficher le contenu d'un répertoire fictif.
+
+## Instructions
+
+### Gestion des Données
+- Le tableau doit être capable d'afficher des données. 
+- Les données seront intégrées directement dans le composant du tableau.
