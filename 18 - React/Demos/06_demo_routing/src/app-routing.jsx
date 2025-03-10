37552a8705178b88ec1b3fa3cba97271a27a8266
CyrilLEDUCQ <cyril.leducq.pro@gmail.com>
2025-03-10 11:48:19 +0100

    Démo Routing

---
+++ 18 - React/Demos/06_demo_routing/src/app-routing.jsx
@@ -0,0 +1,21 @@
+import {createBrowserRouter} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import FormPage from "./pages/FormPage"
+import ErrorPage from "./pages/ErrorPage"
+import NavBar from "./components/NavBar"
+
+const router = createBrowserRouter([
+    // {path: "/", element: <HomePage />, errorElement: <ErrorPage />},
+    // {path: "/form", element: <FormPage />, errorElement : <ErrorPage />}
+    {
+        path: "/",
+        element: <NavBar />,
+        errorElement: <ErrorPage />,
+        children: [
+            {path: "/", element: <HomePage />},
+            {path: "/form", element: <FormPage />}
+        ]
+    }
+])
+
+export default router
