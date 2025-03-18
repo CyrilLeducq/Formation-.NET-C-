de3dd2c289493e5b4f2b914af26b155ec3908a70
CyrilLEDUCQ <cyril.leducq.pro@gmail.com>
2025-03-18 14:46:43 +0100

    Exercie handman + affichage utilisateur reste plus que la partie logique

---
+++ 19 - MAUI/Cours/M2i.Learning.Pendu/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using M2i.LEarning.Pendu.ViewModels;
+
+namespace M2i.LEarning.Pendu
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void btnLetter_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null) return;
+            var letter = button.Text;
+            Debug.WriteLine(letter);
+        }
+    }
+
+}
